528ee9f260af0e776dd0d3549d64ee6f7a808ccd
ronak <ronakmaheshwari077@gmail.com>
2024-08-02 04:31:51 +0530

    Craddle Added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,8 @@ import "../App.css"
 import insta from ".././assets/icons/Instagram.svg"
 import twitter from ".././assets/icons/Twitter.svg"
 import github from ".././assets/icons/Github.svg"
+import linkedin from ".././assets/icons/Linkedin.svg"
+import medium from ".././assets/icons/Medium.svg"
 
 
 
@@ -11,13 +13,14 @@ export default function Footer() {
     <div className='footer mx-auto max-w-screen-xl px-4 py-8 lg:px-8 border-t border-t-slate-400'>
       <div className='sm:flex sm:items-center sm:justify-between'>
         <div className='footer-text text-center text-m text-white lg:mt-0 lg:text-right'>
-          <span>Built with 🖤 by <span><a href='https://chandrabose.live/' target="_blank" rel="noopener noreferrer" style={{textDecoration:"underline" }}>Chandra Bose</a></span></span>
+          <span>Built with 🖤 by <span><a href='https://www.linkedin.com/in/rbm77' target="_blank" rel="noopener noreferrer" style={{textDecoration:"underline" }}>Ronak Maheshwari</a></span></span>
         </div>
         <div className='footer-icons flex justify-center align-middle'>
-          <a href="https://twitter.com/Chandra_Bose31" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="twitter" style={{width:"25px"}} className='mx-5'/></a>
-          <a href="https://github.com/chandrabosep" target="_blank" rel="noopener noreferrer"><img src={github} alt="github" style={{width:"25px"}} className='mx-5'/></a>
-          <a href="https://www.instagram.com/chandrabose.eth/" target="_blank" rel="noopener noreferrer"><img src={insta} alt="instagram" style={{width:"25px"}} className='mx-5'/></a>
-          
+          <a href="https://x.com/0xronak077" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="twitter" style={{width:"25px"}} className='mx-5'/></a>
+          <a href="https://github.com/ronakmaheshwari" target="_blank" rel="noopener noreferrer"><img src={github} alt="github" style={{width:"25px"}} className='mx-5'/></a>
+          <a href="https://www.instagram.com/ronakmaheshwari.eth/" target="_blank" rel="noopener noreferrer"><img src={insta} alt="instagram" style={{width:"25px"}} className='mx-5'/></a>
+          <a href="https://www.linkedin.com/in/rbm77" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="linkedin" style={{width:"25px"}} className='mx-5'/></a>
+          <a href="https://medium.com/@ronakmaheshwari077" target="_blank" rel="noopener noreferrer"><img src={medium} alt="medium" style={{width:"25px"}} className='mx-5'/></a>
         </div>
       </div>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import "../App.css"
 
 export default function Header() {
   return (
-    <div className='flex flex-col text-lg text-slate-200 header' style={{color:"#e2e8f0"}}>
+    <div className='flex flex-col text-lg text-slate-200 header' style={{color:"#fdfdfd"}}>
         <span className=' flex align-self justify-center text-xl mb-9 header-text'>Enjoy the relaxing sounds of your choice.</span>
         <div className='flex align-self justify-center mt-12'>
             <img src={head} alt="headphone" style={{width:"1.7rem"}} />
